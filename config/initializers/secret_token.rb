0c4d5d9b02931d2027e5e969e4af73636d37313b
mostlyclicks <carlos@mostlyclicks.com>
2012-04-16 15:26:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wsu1::Application.config.secret_token = 'd1df9ce6f2929a2cea847aa75fa568eddcf7e1c5102ab65875c5d0c4431277971e782c11619aa45ef6809136d335af46c7304dc5b0c76f36a72df0686bf1dead'
