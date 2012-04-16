ec01e82ed1dd96e1b014e7f064dd9d4a4ce79ece
mostlyclicks <carlos@mostlyclicks.com>
2012-04-16 16:58:14 -0500

    shiv and variable for header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,29 @@
 class PagesController < ApplicationController
+  
+  attr_reader :basic
+  
+  
   def index
+    @header_title = 'Index'
   end
 
   def basic
+    @header_title = 'Basic'
   end
 
   def nav
+    @header_title = 'Navigation'
   end
 
   def tables
+    @header_title = 'Tables'
   end
 
   def forms
+    @header_title = 'Forms'
   end
 
   def alerts
+    @header_title = 'Alerts'
   end
 end
